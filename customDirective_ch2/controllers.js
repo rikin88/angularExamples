4945ece774eca3bf0044d8ea87198a10cd7e0d31
Rikin Asher <rikin.asher@gmail.com>
2014-02-25 14:39:23 -0600

    added new files

---
+++ customDirective_ch2/controllers.js
@@ -0,0 +1,21 @@
+var appModule = angular.module('app', []);
+
+appModule.directive('ngbkFocus', function() {
+	return {
+		link: function(scope, element, attrs, controller) {
+			element[0].focus();
+		}
+	}
+});
+
+function SomeController($scope) {
+	$scope.message = {text: 'nothing clicked yet'};
+
+	$scope.clickUnfocused = function() {
+		$scope.message.text = 'unfocused button clicked';
+	}
+
+	$scope.clickFocused = function() {
+		$scope.message.text = 'focus button clicked';
+	}
+}
